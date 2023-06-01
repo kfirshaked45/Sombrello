311cd53730fb9aa1425c29022c8a9caf1b027249
Omer Hassin <omerhassin21@gmail.com>
2023-06-01 10:57:04 +0300

    added tasks hirarchy

--- frontend/src/cmps/task/task-details.jsx
+++ frontend/src/cmps/task/task-details.jsx
@@ -1,21 +1,25 @@
-// import React, { useState } from 'react';
-// import { AiOutlineEdit } from 'react-icons/ai';
+import React, { useState } from 'react'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { useParams } from 'react-router-dom'
 
-// export function TaskDetails({ task }) {
-//   const [title, setTitle] = useState(task.title);
+export function TaskDetails({ task }) {
+const { boardId, groupId, taskId } = useParams()
+console.log("dfgdfg");
+//   const [title, setTitle] = useState(task.title)
 
 //   const handleTitleChange = (event) => {
-//     const newTitle = event.target.textContent;
-//     setTitle(newTitle);
+//     const newTitle = event.target.textContent
+//     setTitle(newTitle)
 //     // Update the task title using the newTitle value
-//   };
+//   }
 
-//   return (
-//     <>
-//         {/* <AiOutlineEdit size={24} />
-//         <h2 contentEditable="true" onBlur={handleTitleChange} style={{ marginLeft: '10px' }}>
-//             {title}
-//         </h2> */}
-//     </>
-//   )
-// }
+  return (
+    <div>
+      <h1>Task Details</h1>
+      {/* <h1>task._id</h1> */}
+      <p>Board ID: {boardId}</p>
+      <p>Group ID: {groupId}</p>
+      <p>Task ID: {taskId}</p>
+    </div>
+  )
+}
