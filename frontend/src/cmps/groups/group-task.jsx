1be092fe443720f92bdb7854ccf08da2702c96cb
Kfir Shaked <kfirshaked150@gmail.com>
2023-06-01 10:57:13 +0300

    taking omer's changes

--- frontend/src/cmps/groups/group-task.jsx
+++ frontend/src/cmps/groups/group-task.jsx
@@ -2,7 +2,7 @@ export function GroupTask({ task }) {
   console.log(task);
   return (
     <div className="group-task">
-      {/* <textarea value={task.title}></textarea> */}
+      <textarea value={task.title}></textarea>
       {/* <p>{task.status}</p>
       <p>{task.priority}</p> */}
       {/* <p>{task.byMember.fullname}</p> */}
