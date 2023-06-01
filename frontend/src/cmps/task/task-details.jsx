f299bc43dc7e20104e9dee11da1f316c02b2b65b
Omer Hassin <omerhassin21@gmail.com>
2023-06-01 11:39:50 +0300

    changed task preview and details comps

--- frontend/src/cmps/task/task-details.jsx
+++ frontend/src/cmps/task/task-details.jsx
@@ -1,25 +1,21 @@
-import React, { useState } from 'react'
-import { AiOutlineEdit } from 'react-icons/ai'
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation } from "react-router-dom"
 
-export function TaskDetails({ task }) {
-const { boardId, groupId, taskId } = useParams()
-console.log("dfgdfg");
-//   const [title, setTitle] = useState(task.title)
-
-//   const handleTitleChange = (event) => {
-//     const newTitle = event.target.textContent
-//     setTitle(newTitle)
-//     // Update the task title using the newTitle value
-//   }
+export function TaskDetails() {
+  const { boardId, groupId, taskId } = useParams()
+  const location = useLocation()
+  const { task } = location.state || {} // Provide a default value for task if it is null
 
   return (
     <div>
       <h1>Task Details</h1>
-      {/* <h1>task._id</h1> */}
-      <p>Board ID: {boardId}</p>
-      <p>Group ID: {groupId}</p>
-      <p>Task ID: {taskId}</p>
+      {task && (
+        <div>
+          <h1>{task.id}</h1>
+          <p>Task ID: {taskId}</p>
+          <p>Group ID: {groupId}</p>
+          <p>Board ID: {boardId}</p>
+        </div>
+      )}
     </div>
   )
 }
