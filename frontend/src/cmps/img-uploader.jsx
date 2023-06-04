bfab954014883ed8436f40fa6a817168fa69ad67
Kfir Shaked <kfirshaked150@gmail.com>
2023-06-04 20:43:02 +0300

    CHANGED states

--- frontend/src/cmps/img-uploader.jsx
+++ frontend/src/cmps/img-uploader.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import { uploadService } from '../services/upload.service'
+import { useState } from 'react';
+import { uploadService } from '../services/upload.service';
 
 export function ImgUploader({ onUploaded = null }) {
   const [imgData, setImgData] = useState({
     imgUrl: null,
     height: 500,
     width: 500,
-  })
-  const [isUploading, setIsUploading] = useState(false)
+  });
+  const [isUploading, setIsUploading] = useState(false);
 
   async function uploadImg(ev) {
-    setIsUploading(true)
-    const { secure_url, height, width } = await uploadService.uploadImg(ev)
-    setImgData({ imgUrl: secure_url, width, height })
-    setIsUploading(false)
-    onUploaded && onUploaded(secure_url)
+    setIsUploading(true);
+    const { secure_url, height, width } = await uploadService.uploadImg(ev);
+    setImgData({ imgUrl: secure_url, width, height });
+    setIsUploading(false);
+    onUploaded && onUploaded(secure_url);
   }
 
   function getUploadLabel() {
-    if (imgData.imgUrl) return 'Upload Another?'
-    return isUploading ? 'Uploading....' : 'Upload Image'
+    if (imgData.imgUrl) return 'Upload Another?';
+    return isUploading ? 'Uploading....' : 'Upload From Computer';
   }
 
   return (
@@ -28,5 +28,5 @@ export function ImgUploader({ onUploaded = null }) {
       <label htmlFor="imgUpload">{getUploadLabel()}</label>
       <input type="file" onChange={uploadImg} accept="img/*" id="imgUpload" />
     </div>
-  )
+  );
 }
