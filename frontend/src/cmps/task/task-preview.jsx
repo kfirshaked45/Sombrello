f9e723716c472f6e25c81aeccc2f6bbf9fb0dd5b
Kfir Shaked <kfirshaked150@gmail.com>
2023-06-05 12:47:00 +0300

    fixed img

--- frontend/src/cmps/task/task-preview.jsx
+++ frontend/src/cmps/task/task-preview.jsx
@@ -1,12 +1,11 @@
 export function TaskPreview({ groupId, task }) {
-  const imageUrl = task.attachments[0]
+  if (!task.attachments) return;
+  const imageUrl = task.attachments[0];
 
   return (
     <div className="task-preview">
-      {imageUrl && (
-        <img src={imageUrl} alt="Task Image" className="task-image" />
-      )}
+      {imageUrl && <img src={imageUrl} alt="Task Image" className="task-image" />}
       <span className="task-item-title">{task.title}</span>
     </div>
-  )
+  );
 }
