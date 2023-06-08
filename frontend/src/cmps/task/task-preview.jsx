9c0cdd7d65e6e320d02767ac8e2232ea036e3ad9
Kfir Shaked <kfirshaked150@gmail.com>
2023-06-08 19:01:57 +0300

    fixed merge

--- frontend/src/cmps/task/task-preview.jsx
+++ frontend/src/cmps/task/task-preview.jsx
@@ -1,70 +1,50 @@
-import { utilService } from "../../services/util.service"
-import { useDispatch } from "react-redux"
-import { updateBoard } from "../../store/board.actions"
-import { RiAttachment2 } from "react-icons/ri"
-import { AiOutlineClockCircle } from "react-icons/ai"
-import { TfiAlignLeft } from "react-icons/tfi"
-import { GoComment } from "react-icons/go"
-import { TaskDueDate } from "./task-due-date"
-import { useParams } from "react-router"
-import { boardService } from "../../services/board.service.local"
+import { utilService } from '../../services/util.service';
+import { useDispatch } from 'react-redux';
+import { updateBoard } from '../../store/board.actions';
+import { RiAttachment2 } from 'react-icons/ri';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+import { TfiAlignLeft } from 'react-icons/tfi';
+import { GoComment } from 'react-icons/go';
+import { TaskDueDate } from './task-due-date';
+import { useParams } from 'react-router';
+import { boardService } from '../../services/board.service.local';
 
 export function TaskPreview({ board, groupId, task }) {
-  const imageUrl = task.attachments && task.attachments[0]
-  const labels = task.labels
-  const color = task.style && task.style.coverColor
-  const description = task.desc
+  const imageUrl = task.attachments && task.attachments[0];
+  const labels = task.labels;
+  const color = task.style && task.style.coverColor;
+  const description = task.desc;
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   function toggleIsDone(ev) {
-    ev.stopPropagation() // Stop event propagation
+    ev.stopPropagation(); // Stop event propagation
 
-    const updatedDueDate = { ...task.dueDate, isDone: !task.dueDate.isDone }
-    const updatedTask = { ...task, dueDate: updatedDueDate }
+    const updatedDueDate = { ...task.dueDate, isDone: !task.dueDate.isDone };
+    const updatedTask = { ...task, dueDate: updatedDueDate };
     const updatedGroups = board.groups.map((group) => {
       if (group.id === groupId) {
         const updatedGroup = {
           ...group,
           tasks: group.tasks.map((currTask) => (currTask.id === task.id ? updatedTask : currTask)),
-        }
-        return updatedGroup
+        };
+        return updatedGroup;
       }
-      return group
-    })
-    const updatedBoard = { ...board, groups: updatedGroups }
-    dispatch(updateBoard(updatedBoard))
+      return group;
+    });
+    const updatedBoard = { ...board, groups: updatedGroups };
+    dispatch(updateBoard(updatedBoard));
   }
 
   return (
     <div className="cover-img-section">
-      {color && !imageUrl && (
-        <div
-          className="task-list-cover"
-          style={{ backgroundColor: color }}
-        ></div>
-      )}
-      {imageUrl && (
-        <img src={imageUrl} alt="Task Image" className="task-image" />
-      )}
-      {color && !imageUrl && (
-        <div
-          className="task-list-cover"
-          style={{ backgroundColor: color }}
-        ></div>
-      )}
-      {imageUrl && (
-        <img src={imageUrl} alt="Task Image" className="task-image" />
-      )}
+      {imageUrl && <img src={imageUrl} alt="Task Image" className="task-image" />}
+      {color && !imageUrl && <div className="task-list-cover" style={{ backgroundColor: color }}></div>}
       <div className="task-preview">
         {labels && labels.length !== 0 && (
           <div className="task-preview-labels">
             {labels.map((label, index) => (
-              <button
-                key={index}
-                style={{ backgroundColor: label.color }}
-                className="group-label"
-              ></button>
+              <button key={index} style={{ backgroundColor: label.color }} className="group-label"></button>
             ))}
           </div>
         )}
@@ -79,9 +59,7 @@ export function TaskPreview({ board, groupId, task }) {
             (task.members && task.members.length !== 0)) && (
             <section className="task-item-footer">
               <div className="props-icons" onClick={toggleIsDone}>
-                {task.dueDate && (
-                  <TaskDueDate dueDate={task.dueDate} onClick={toggleIsDone} />
-                )}
+                {task.dueDate && <TaskDueDate dueDate={task.dueDate} onClick={toggleIsDone} />}
 
                 {description && (
                   <section className="description-icon">
@@ -92,18 +70,14 @@ export function TaskPreview({ board, groupId, task }) {
                 {task.comments && task.comments.length !== 0 && (
                   <section className="comments-icon">
                     <GoComment />
-                    <span className="comments-count">
-                      {task.comments.length}
-                    </span>
+                    <span className="comments-count">{task.comments.length}</span>
                   </section>
                 )}
 
                 {task.attachments && task.attachments.length !== 0 && (
                   <section className="attachments-icon">
                     <RiAttachment2 />
-                    <span className="attachments-count">
-                      {task.attachments.length}
-                    </span>
+                    <span className="attachments-count">{task.attachments.length}</span>
                   </section>
                 )}
               </div>
@@ -112,11 +86,7 @@ export function TaskPreview({ board, groupId, task }) {
                   <section className="members-img">
                     {task.members.map((member) => (
                       <div className="member-img" key={member._id}>
-                        <img
-                          src={member.imgUrl}
-                          alt=""
-                          referrerPolicy="no-referrer"
-                        />
+                        <img src={member.imgUrl} alt="" referrerPolicy="no-referrer" />
                       </div>
                     ))}
                   </section>
@@ -126,5 +96,5 @@ export function TaskPreview({ board, groupId, task }) {
           )}
       </div>
     </div>
-  )
+  );
 }
