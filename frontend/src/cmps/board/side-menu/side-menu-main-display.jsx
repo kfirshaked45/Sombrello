d06d0559254b8955b9f4f9cee9f8d4db2032bd9f
Ofek Rashti <ofekr2261@gmail.com>
2023-06-08 15:13:31 +0300

    styling confirmation box

--- frontend/src/cmps/board/side-menu/side-menu-main-display.jsx
+++ frontend/src/cmps/board/side-menu/side-menu-main-display.jsx
@@ -3,6 +3,7 @@ import { useMatch } from 'react-router'
 import { removeBoard } from '../../../store/board.actions'
 import { useNavigate } from 'react-router-dom'
 import { confirmAlert } from 'react-confirm-alert'
+import '../../../assets/styles/cmps/board/_confirm-alert.scss'
 import 'react-confirm-alert/src/react-confirm-alert.css'
 
 export const SideMenuMainDisplay = ({ onChangeTitle }) => {
