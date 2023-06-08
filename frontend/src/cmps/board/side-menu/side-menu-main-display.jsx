7302332fdaf47a566a93708bd60f216f6ad9a7da
Ofek Rashti <ofekr2261@gmail.com>
2023-06-08 14:26:56 +0300

    adding delete board feature

--- frontend/src/cmps/board/side-menu/side-menu-main-display.jsx
+++ frontend/src/cmps/board/side-menu/side-menu-main-display.jsx
@@ -1,7 +1,42 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useMatch } from 'react-router'
+import { removeBoard } from '../../../store/board.actions'
+import { useNavigate } from 'react-router-dom'
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 export const SideMenuMainDisplay = ({ onChangeTitle }) => {
-  const board = useSelector((state) => state.boardModule.board)
+  const match = useMatch('/board/:boardId')
+  const boardId = match?.params?.boardId
+  const boards = useSelector((state) => state.boardModule.boards)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleRemoveBoard = async (boardId) => {
+    confirmAlert({
+      title: 'Confirm Removal',
+      message:
+        'Are you sure you want to remove this board? All of your data will be lost',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: async () => {
+            try {
+              await dispatch(removeBoard(boardId))
+              console.log('Board removed successfully')
+              navigate('/workspace')
+            } catch (err) {
+              console.log('Error removing board:', err)
+            }
+          },
+        },
+        {
+          label: 'No',
+          onClick: () => {},
+        },
+      ],
+    })
+  }
 
   return (
     <section className="side-menu-main-display">
@@ -9,6 +44,15 @@ export const SideMenuMainDisplay = ({ onChangeTitle }) => {
         <button onClick={() => onChangeTitle('Change background')}>
           Change background
         </button>
+        {boards.map((board) => (
+          <div key={board._id}>
+            {board._id === boardId && (
+              <button onClick={() => handleRemoveBoard(board._id)}>
+                Remove Board
+              </button>
+            )}
+          </div>
+        ))}
       </section>
     </section>
   )
--- frontend/src/cmps/task/task-preview.jsx
+++ frontend/src/cmps/task/task-preview.jsx
@@ -1,99 +1,121 @@
-import { utilService } from '../../services/util.service';
-import { useDispatch } from 'react-redux';
-import { updateBoard } from '../../store/board.actions';
-import { RiAttachment2 } from 'react-icons/ri';
-import { AiOutlineClockCircle } from 'react-icons/ai';
-import { TfiAlignLeft } from 'react-icons/tfi';
-import { GoComment } from 'react-icons/go';
-import { TaskDueDate } from './task-due-date';
-import { useParams } from 'react-router';
-import { boardService } from '../../services/board.service.local';
+import { utilService } from '../../services/util.service'
+import { useDispatch } from 'react-redux'
+import { updateBoard } from '../../store/board.actions'
+import { RiAttachment2 } from 'react-icons/ri'
+import { AiOutlineClockCircle } from 'react-icons/ai'
+import { TfiAlignLeft } from 'react-icons/tfi'
+import { GoComment } from 'react-icons/go'
+import { TaskDueDate } from './task-due-date'
+import { useParams } from 'react-router'
+import { boardService } from '../../services/board.service.local'
 
 export function TaskPreview({ board, groupId, task }) {
-  const imageUrl = task.attachments && task.attachments[0];
-  const labels = task.labels;
-  const color = task.style && task.style.coverColor;
-  const description = task.desc;
+  const imageUrl = task.attachments && task.attachments[0]
+  const labels = task.labels
+  const color = task.style && task.style.coverColor
+  const description = task.desc
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   function toggleIsDone(ev) {
-    ev.stopPropagation(); // Stop event propagation
+    ev.stopPropagation() // Stop event propagation
 
-    const updatedDueDate = { ...task.dueDate, isDone: !task.dueDate.isDone };
-    const updatedTask = { ...task, dueDate: updatedDueDate };
+    const updatedDueDate = { ...task.dueDate, isDone: !task.dueDate.isDone }
+    const updatedTask = { ...task, dueDate: updatedDueDate }
     const updatedGroups = board.groups.map((g) => {
       if (g.id === groupId) {
         const updatedGroup = {
           ...g,
           tasks: g.tasks.map((t) => (t.id === task.id ? updatedTask : t)),
-        };
-        return updatedGroup;
+        }
+        return updatedGroup
       }
-      return g;
-    });
-    const updatedBoard = { ...board, groups: updatedGroups };
-    dispatch(updateBoard(updatedBoard));
+      return g
+    })
+    const updatedBoard = { ...board, groups: updatedGroups }
+    dispatch(updateBoard(updatedBoard))
   }
 
   return (
     <div className="cover-img-section">
-      {color && !imageUrl && <div className="task-list-cover" style={{ backgroundColor: color }}></div>}
-      {imageUrl && <img src={imageUrl} alt="Task Image" className="task-image" />}
+      {color && !imageUrl && (
+        <div
+          className="task-list-cover"
+          style={{ backgroundColor: color }}
+        ></div>
+      )}
+      {imageUrl && (
+        <img src={imageUrl} alt="Task Image" className="task-image" />
+      )}
       <div className="task-preview">
         {labels && labels.length !== 0 && (
           <div className="task-preview-labels">
             {labels.map((label, index) => (
-              <button key={index} style={{ backgroundColor: label.color }} className="group-label"></button>
+              <button
+                key={index}
+                style={{ backgroundColor: label.color }}
+                className="group-label"
+              ></button>
             ))}
           </div>
         )}
 
         <span className="task-item-title">{task.title}</span>
 
-        {(task.dueDate.timeStamp ||
-          description ||
-          (task.comments && task.comments.length !== 0) ||
-          (task.attachments && task.attachments.length !== 0) ||
-          (task.members && task.members.length !== 0)) && (
-          <section className="task-item-footer">
-            <div className="props-icons" onClick={toggleIsDone}>
-              {task.dueDate && <TaskDueDate dueDate={task.dueDate} onClick={toggleIsDone} />}
+        {task.dueDate &&
+          task.dueDate.timeStamp &&
+          (description ||
+            (task.comments && task.comments.length !== 0) ||
+            (task.attachments && task.attachments.length !== 0) ||
+            (task.members && task.members.length !== 0)) && (
+            <section className="task-item-footer">
+              <div className="props-icons" onClick={toggleIsDone}>
+                {task.dueDate && (
+                  <TaskDueDate dueDate={task.dueDate} onClick={toggleIsDone} />
+                )}
 
-              {description && (
-                <section className="description-icon">
-                  <TfiAlignLeft />
-                </section>
-              )}
+                {description && (
+                  <section className="description-icon">
+                    <TfiAlignLeft />
+                  </section>
+                )}
 
-              {task.comments && task.comments.length !== 0 && (
-                <section className="comments-icon">
-                  <GoComment />
-                  <span className="comments-count">{task.comments.length}</span>
-                </section>
-              )}
+                {task.comments && task.comments.length !== 0 && (
+                  <section className="comments-icon">
+                    <GoComment />
+                    <span className="comments-count">
+                      {task.comments.length}
+                    </span>
+                  </section>
+                )}
 
-              {task.attachments && task.attachments.length !== 0 && (
-                <section className="attachments-icon">
-                  <RiAttachment2 />
-                  <span className="attachments-count">{task.attachments.length}</span>
-                </section>
-              )}
-            </div>
-            <div className="members-icons">
-              {task.members && task.members.length !== 0 && (
-                <section className="members-img">
-                  {task.members.map((member) => (
-                    <div className="member-img" key={member._id}>
-                      <img src={member.imgUrl} alt="" referrerPolicy="no-referrer" />
-                    </div>
-                  ))}
-                </section>
-              )}
-            </div>
-          </section>
-        )}
+                {task.attachments && task.attachments.length !== 0 && (
+                  <section className="attachments-icon">
+                    <RiAttachment2 />
+                    <span className="attachments-count">
+                      {task.attachments.length}
+                    </span>
+                  </section>
+                )}
+              </div>
+              <div className="members-icons">
+                {task.members && task.members.length !== 0 && (
+                  <section className="members-img">
+                    {task.members.map((member) => (
+                      <div className="member-img" key={member._id}>
+                        <img
+                          src={member.imgUrl}
+                          alt=""
+                          referrerPolicy="no-referrer"
+                        />
+                      </div>
+                    ))}
+                  </section>
+                )}
+              </div>
+            </section>
+          )}
       </div>
     </div>
-  );
+  )
 }
