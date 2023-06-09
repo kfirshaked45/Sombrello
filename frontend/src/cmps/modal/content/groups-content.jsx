f5a2b3cb8a8834109d75d3c62cef7af4ebb8c127
Ofek Rashti <ofekr2261@gmail.com>
2023-06-09 19:26:49 +0300

    adding copy list feature

--- frontend/src/cmps/modal/content/groups-content.jsx
+++ frontend/src/cmps/modal/content/groups-content.jsx
@@ -1,27 +1,53 @@
-import { updateBoard } from '../../../store/board.actions';
+import { useState } from 'react'
+import { updateBoard } from '../../../store/board.actions'
+import { utilService } from '../../../services/util.service'
 
 export function GroupsContent({ group, board, dispatch }) {
-  async function deleteGroup() {
-    const updatedGroups = board.groups.filter((g) => g.id !== group.id);
-    const updatedBoard = { ...board, groups: updatedGroups };
+  const [showCopyListModal, setShowCopyListModal] = useState(false)
 
-    await dispatch(updateBoard(updatedBoard));
+  async function deleteGroup() {
+    const updatedGroups = board.groups.filter((g) => g.id !== group.id)
+    const updatedBoard = { ...board, groups: updatedGroups }
+
+    await dispatch(updateBoard(updatedBoard))
+  }
+
+  async function copyList() {
+    const newListName = document.getElementById('newListNameInput').value
+
+    const newList = { ...group, id: utilService.makeId(), title: newListName }
+
+    const updatedBoard = { ...board, groups: [...board.groups, newList] }
+
+    await dispatch(updateBoard(updatedBoard))
+
+    setShowCopyListModal(false)
   }
 
   return (
     <div className="group-edit-modal">
       <div className="group-modal-top">
-        <button>Copy list...</button>
+        <button onClick={() => setShowCopyListModal(true)}>Copy list...</button>
         <button>Add card...</button>
         <button>Move list...</button>
         <button>Watch</button>
       </div>
-      {/* <div> */}
-
       <button>Sort by</button>
-      {/* </div> */}
       <button onClick={deleteGroup}>Delete this list</button>
       <button>Add card...</button>
+
+      {showCopyListModal && (
+        <div className="copy-list-modal">
+          <h3>Copy List</h3>
+          <input
+            type="text"
+            id="newListNameInput"
+            placeholder="Enter list name"
+          />
+          <button onClick={copyList}>Copy</button>
+          <button onClick={() => setShowCopyListModal(false)}>Cancel</button>
+        </div>
+      )}
     </div>
-  );
+  )
 }
