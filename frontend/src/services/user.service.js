6f2175d09f88dbd74db3c1dcbdba52239d7d0d7c
Ofek Rashti <ofekr2261@gmail.com>
2023-06-12 19:45:01 +0300

    fixing user login

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,6 +1,33 @@
 import { httpService } from './http.service'
 import { utilService } from './util.service'
 import userImg from '../assets/img/user-img.jpg'
+import omerImg from '../assets/img/omer-img.jpg'
+import ofekImg from '../assets/img/user-img1.jpg'
+import kfirImg from '../assets/img/kfirImg.jpg'
+
+const fixedMembers = [
+  {
+    _id: 'u101',
+    fullname: 'Kfir Shaked',
+    userName: 'Kfir',
+    password: 'Kfir123',
+    imgUrl: kfirImg,
+  },
+  {
+    _id: 'u102',
+    fullname: 'Ofek Rashti',
+    userName: 'Ofek',
+    password: 'ofekr2261',
+    imgUrl: ofekImg,
+  },
+  {
+    _id: 'u103',
+    fullname: 'Omer Hassin',
+    userName: 'Omer',
+    password: 'omer123',
+    imgUrl: omerImg,
+  },
+]
 
 export const userService = {
   getUsers,
@@ -18,11 +45,26 @@ const BASE_URL = `user/`
 async function login(credentials, isGoogleAuth) {
   try {
     let user
-    if (isGoogleAuth)
+    if (isGoogleAuth) {
       user = await httpService.post('google-auth/login', credentials)
-    else user = await httpService.post('auth/login', credentials)
+    } else {
+      const fixedMember = fixedMembers.find(
+        (member) =>
+          member.userName === credentials.username &&
+          member.password === credentials.password
+      )
+      if (!fixedMember) {
+        throw new Error('Invalid credentials')
+      }
+      user = {
+        _id: fixedMember._id,
+        fullname: fixedMember.fullname,
+        username: fixedMember.userName,
+        imgUrl: fixedMember.imgUrl,
+      }
+    }
 
-    if (user) sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
     return user
   } catch (err) {
     console.log('Cannot login:', err)
@@ -45,7 +87,14 @@ async function signup(credentials, isGoogleAuth) {
     if (isGoogleAuth) {
       user = await httpService.post('google-auth/signup', credentials)
     } else {
-      credentials.imgUrl = GUEST_IMG_URL
+      const fixedMember = fixedMembers.find(
+        (member) => member.fullname === credentials.fullname
+      )
+      if (fixedMember) {
+        credentials.imgUrl = fixedMember.imgUrl
+      } else {
+        credentials.imgUrl = GUEST_IMG_URL
+      }
       user = await httpService.post('auth/signup', credentials)
     }
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
@@ -58,14 +107,26 @@ async function signup(credentials, isGoogleAuth) {
 }
 
 function getLoggedInUser() {
-  return (
-    JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN)) || {
-      fullname: 'Guest',
-      username: 'Guest',
-      imgUrl: GUEST_IMG_URL,
-      _id: utilService.makeId(),
+  const loggedInUser = JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN))
+
+  if (loggedInUser) {
+    const fixedMember = fixedMembers.find(
+      (member) => member._id === loggedInUser._id
+    )
+    if (fixedMember) {
+      return {
+        ...loggedInUser,
+        imgUrl: fixedMember.imgUrl,
+      }
     }
-  )
+  }
+
+  return {
+    fullname: 'Guest',
+    username: 'Guest',
+    imgUrl: GUEST_IMG_URL,
+    _id: utilService.makeId(),
+  }
 }
 
 async function getUsers() {
