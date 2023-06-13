5246efd71ca45eee8645aa5aa293780c1402203a
Ofek Rashti <ofekr2261@gmail.com>
2023-06-13 16:25:45 +0300

    editing activity

--- frontend/src/cmps/activity-details.jsx
+++ frontend/src/cmps/activity-details.jsx
@@ -1,4 +1,4 @@
-import { RxActivityLog } from 'react-icons/rx';
+import { RxActivityLog } from 'react-icons/rx'
 
 export function ActivityDetails({ board }) {
   // activitiy - byMember - id - createdAt , text -
@@ -6,37 +6,68 @@ export function ActivityDetails({ board }) {
   //
   //
   function formatCreatedAt(createdAt) {
-    const now = new Date();
-    const createdDate = new Date(createdAt);
+    const now = new Date()
+    const createdDate = new Date(createdAt)
 
-    const diff = now.getTime() - createdDate.getTime();
-    const diffMinutes = Math.floor(diff / (1000 * 60));
-    const diffHours = Math.floor(diff / (1000 * 60 * 60));
-    const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const diff = now.getTime() - createdDate.getTime()
+    const diffMinutes = Math.floor(diff / (1000 * 60))
+    const diffHours = Math.floor(diff / (1000 * 60 * 60))
+    const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24))
 
-    if (diffMinutes < 60) {
-      return `${diffMinutes} min${diffMinutes !== 1 ? 's' : ''} ago`;
+    if (diffMinutes < 1) {
+      return 'Right now'
+    } else if (diffMinutes < 60) {
+      return `${diffMinutes} min${diffMinutes !== 1 ? 's' : ''} ago`
     } else if (diffHours < 24) {
-      return `an hour${diffHours !== 1 ? 's' : ''} ago`;
+      return `an hour${diffHours !== 1 ? 's' : ''} ago`
     } else if (diffDays === 1) {
-      return `yesterday at${createdDate.getHours()}:${createdDate.getMinutes()}`;
+      return `yesterday at ${createdDate.getHours()}:${createdDate.getMinutes()}`
     } else if (now.getFullYear() === createdDate.getFullYear()) {
-      const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      const month = monthNames[createdDate.getMonth()];
-      const day = createdDate.getDate();
-      const hour = createdDate.getHours();
-      const minutes = createdDate.getMinutes();
-      return `${month} ${day} at ${hour}:${minutes}`;
+      const monthNames = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+      ]
+      const month = monthNames[createdDate.getMonth()]
+      const day = createdDate.getDate()
+      const hour = createdDate.getHours()
+      const minutes = createdDate.getMinutes()
+      return `${month} ${day} at ${hour}:${minutes}`
     } else {
-      const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      const month = monthNames[createdDate.getMonth()];
-      const day = createdDate.getDate();
-      const hour = createdDate.getHours();
-      const minutes = createdDate.getMinutes();
-      return `${month} ${day}, ${createdDate.getFullYear()} at ${hour}:${minutes}`;
+      const monthNames = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+      ]
+      const month = monthNames[createdDate.getMonth()]
+      const day = createdDate.getDate()
+      const hour = createdDate.getHours()
+      const minutes = createdDate.getMinutes()
+      return `${month} ${day}, ${createdDate.getFullYear()} at ${hour}:${minutes}`
     }
   }
-  const sortedActivities = [...board.activities].sort((a, b) => b.createdAt - a.createdAt);
+
+  const sortedActivities = [...board.activities].sort(
+    (a, b) => b.createdAt - a.createdAt
+  )
   return (
     <div className="activity-details">
       <button className="activity-top-container">
@@ -49,15 +80,23 @@ export function ActivityDetails({ board }) {
         {sortedActivities.length > 0 &&
           sortedActivities.map((activity) => (
             <div key={activity.id} className="activity-user-container">
-              <img src={activity.byMember.imgUrl} alt="user-img" className="activity-user-img" />
+              <img
+                src={activity.byMember.imgUrl}
+                alt="user-img"
+                className="activity-user-img"
+              />
               <div className="activity-user-text-container">
-                <span className="activity-username">{activity.byMember.username}</span>
+                <span className="activity-username">
+                  {activity.byMember.username}
+                </span>
                 <span className="activity-text">{activity.text} </span>
               </div>
-              <div className="activity-created-at">{formatCreatedAt(activity.createdAt)}</div>
+              <div className="activity-created-at">
+                {formatCreatedAt(activity.createdAt)}
+              </div>
             </div>
           ))}
       </div>
     </div>
-  );
+  )
 }
