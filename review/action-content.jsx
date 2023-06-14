2e5cbc5b220dee7b570b06d27915ed6628fb883c
Kfir Shaked <kfirshaked150@gmail.com>
2023-06-14 12:41:54 +0300

    new

--- review/action-content.jsx
+++ review/action-content.jsx
@@ -6,19 +6,19 @@ const actionComponents = {
   Attachments: AttachmentsContent,
   Group: GroupsContent,
   Cover: CoverContent,
-}
+};
 
 export function ActionContent({ action, board, task, group, attachmentId, onClose, labelId, modalRef }) {
-  const dispatch = useDispatch()
-  const ContentComponent = actionComponents[action] || null
-  const loggedInUser = useSelector((storeState) => storeState.userModule.user)
+  const dispatch = useDispatch();
+  const ContentComponent = actionComponents[action] || null;
+  const loggedInUser = useSelector((storeState) => storeState.userModule.user);
   function createActivity(text) {
     return {
       id: utilService.makeId(),
       text,
       createdAt: Date.now(),
       byMember: loggedInUser,
-    }
+    };
   }
 
   return ContentComponent ? (
@@ -33,5 +33,5 @@ export function ActionContent({ action, board, task, group, attachmentId, onClos
       modalRef={modalRef}
       createActivity={createActivity}
     />
-  ) : null
+  ) : null;
 }
