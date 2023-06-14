bafa4d50fab00d44da2537ba61415738251bffb6
Ofek Rashti <ofekr2261@gmail.com>
2023-06-14 13:32:47 +0300

    updating fixedmembers

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,12 +1,5 @@
 import { httpService } from './http.service'
 import { utilService } from './util.service'
-import userImg from '../assets/img/user-img.jpg'
-import omerImg from '../assets/img/omer-img.jpg'
-import ofekImg from '../assets/img/user-img1.jpg'
-import kfirImg from '../assets/img/kfirImg.jpg'
-import batelImg from '../assets/img/batelImg.jpg'
-import danielImg from '../assets/img/danielImg.png'
-import tommyImg from '../assets/img/tommyImg.jpg'
 
 const fixedMembers = [
   {
@@ -14,42 +7,48 @@ const fixedMembers = [
     fullname: 'Kfir Shaked',
     userName: 'Kfir',
     password: 'Kfir123',
-    imgUrl: kfirImg,
+    imgUrl:
+      'https://res.cloudinary.com/dcaqp34xm/image/upload/v1686731130/kfirImg_ekhawt.jpg',
   },
   {
     _id: 'u102',
     fullname: 'Ofek Rashti',
     userName: 'Ofek',
     password: 'ofekr2261',
-    imgUrl: ofekImg,
+    imgUrl:
+      'https://res.cloudinary.com/dcaqp34xm/image/upload/v1686731130/user-img1_efdss2.jpg',
   },
   {
     _id: 'u103',
     fullname: 'Omer Hassin',
     userName: 'Omer',
     password: 'omer123',
-    imgUrl: omerImg,
+    imgUrl:
+      'https://res.cloudinary.com/dcaqp34xm/image/upload/v1686731130/omer-img_lf2bdx.jpg',
   },
   {
     _id: 'u104',
     fullname: 'Batel Katiei',
     userName: 'Batel',
     password: 'batel123',
-    imgUrl: batelImg,
+    imgUrl:
+      'https://res.cloudinary.com/dcaqp34xm/image/upload/v1686731130/batelImg_f9evg0.jpg',
   },
   {
     _id: 'u105',
     fullname: 'Daniel Shaked',
     userName: 'Daniel',
     password: 'daniel123',
-    imgUrl: danielImg,
+    imgUrl:
+      'https://res.cloudinary.com/dcaqp34xm/image/upload/v1686731130/danielImg_pyvfbh.png',
   },
   {
     _id: 'u106',
     fullname: 'Tommy Irmia',
     userName: 'Tommy',
     password: 'tommy123',
-    imgUrl: tommyImg,
+    imgUrl:
+      'https://res.cloudinary.com/dcaqp34xm/image/upload/v1686731130/tommyImg_y0eq3l.jpg',
   },
 ]
 
