53ef06f843c6f5c965aa6e34a83b30c517cbf328
Kfir Shaked <kfirshaked150@gmail.com>
2023-06-14 11:28:59 +0300

    te

--- frontend/src/pages/board-details.jsx
+++ frontend/src/pages/board-details.jsx
@@ -1,71 +1,63 @@
-import { showErrorMsg } from '../services/event-bus.service'
-import { BoardHeader } from '../cmps/board/board-header'
-import { GroupList } from '../cmps/groups/group-list'
-import { boardService } from '../services/board.service.local'
-import {
-  getActionUpdateBoard,
-  loadBoards,
-  updateBoard,
-} from '../store/board.actions'
-import { useDispatch, useSelector } from 'react-redux'
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
-import { socketService } from '../services/socket.service.js'
+import { showErrorMsg } from '../services/event-bus.service';
+import { BoardHeader } from '../cmps/board/board-header';
+import { GroupList } from '../cmps/groups/group-list';
+import { boardService } from '../services/board.service.local';
+import { getActionUpdateBoard, loadBoards, updateBoard } from '../store/board.actions';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { socketService } from '../services/socket.service.js';
 
 export function BoardDetails() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { boardId } = useParams()
-  const boards = useSelector((state) => state.boardModule.boards)
-  const loggedInUser = useSelector((storeState) => storeState.userModule.user)
-  const board = boards.find((b) => b._id === boardId)
-  const [socketConnected, setSocketConnected] = useState(false)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { boardId } = useParams();
+  const boards = useSelector((state) => state.boardModule.boards);
+  const loggedInUser = useSelector((storeState) => storeState.userModule.user);
+  const board = boards.find((b) => b._id === boardId);
+  const [socketConnected, setSocketConnected] = useState(false);
 
   useEffect(() => {
-    dispatch(loadBoards())
-    socketService.setup() // Call the setup function before connecting
+    dispatch(loadBoards());
+    socketService.setup(); // Call the setup function before connecting
 
     socketService.on('connect', () => {
-      setSocketConnected(true)
-      socketService.emit('join-board', boardId) // Join the specific board room
-    })
+      setSocketConnected(true);
+      socketService.emit('join-board', boardId); // Join the specific board room
+    });
 
     socketService.on('disconnect', () => {
-      setSocketConnected(false)
-    })
+      setSocketConnected(false);
+    });
 
     socketService.on('update-board', (updatedBoard) => {
-      console.log(
-        '🚀 ~ file: board-details.jsx:40 ~ socketService.on ~ updatedBoard:',
-        updatedBoard
-      )
-      dispatch(getActionUpdateBoard(updatedBoard))
-    })
+      console.log('🚀 ~ file: board-details.jsx:40 ~ socketService.on ~ updatedBoard:', updatedBoard);
+      dispatch(getActionUpdateBoard(updatedBoard));
+    });
 
     return () => {
-      socketService.disconnect()
-    }
-  }, [boardId, dispatch])
+      socketService.disconnect();
+    };
+  }, [boardId, dispatch]);
 
   const getBoardStyle = () => {
-    if (!board) return
+    if (!board) return;
     if (board?.style.background)
       return {
         background: `url('${board.style.background}') center center / cover`,
-      }
-    else if (board?.style.backgroundColor)
-      return { backgroundColor: `${board.style.backgroundColor}` }
+      };
+    else if (board?.style.backgroundColor) return { backgroundColor: `${board.style.backgroundColor}` };
     // In case there are no background image and color
-    return { backgroundColor: `pink` }
-  }
+    return { backgroundColor: `pink` };
+  };
 
-  const boardStyle = getBoardStyle()
+  const boardStyle = getBoardStyle();
 
   const changeBackground = ({ background, backgroundColor, thumbnail }) => {
-    board.style = { background, backgroundColor, thumbnail }
-    dispatch(updateBoard(board))
-    socketService.emit('update-board', board)
-  }
+    board.style = { background, backgroundColor, thumbnail };
+    dispatch(updateBoard(board));
+    socketService.emit('update-board', board);
+  };
 
   return (
     <main className="board" style={boardStyle}>
@@ -73,11 +65,7 @@ export function BoardDetails() {
       <div className="board-details">
         <GroupList board={board} loggedInUser={loggedInUser} />
       </div>
-      {socketConnected ? (
-        <div>Socket connected</div>
-      ) : (
-        <div>Socket disconnected</div>
-      )}
+
     </main>
-  )
+  );
 }
